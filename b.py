fa987c24411f970c42391e018494680835481e14
SUMIOKU Hibiki <117646103+hibichy@users.noreply.github.com>
2023-04-17 18:16:37 +0900

    Update b.py

--- b.py
+++ b.py
@@ -8,6 +8,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import scipy.stats as stats
 
+#画面のタイトルをつける
+st.markdown("<h1 style='text-align: center;'>回帰分析アプリ</h1>", unsafe_allow_html=True)
+
 # ファイルアップロード機能を実装する
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
